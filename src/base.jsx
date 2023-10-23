3aa252d4dc8c6f12bef0336f4e3e9ef8fa2e4f5d
Максим Русеев <maxim0ruseev@gmail.com>
2023-10-24 02:26:01 +0400

    24.10

--- src/base.jsx
+++ src/base.jsx
@@ -1,63 +1,96 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Base() {
     let [str, setStr] = useState('Пусто') //вывод
     let [mat, setMat] = useState([]) //процесс
     let [sum, setSum] = useState('') //результат
-    let [test, setTest] = useState([11,1,3])
-    let [test2, setTest2] = useState(['-', '+', '+'])
+    let [test, setTest] = useState([])
+    let [test2, setTest2] = useState([])
     let [bool, setBool] = useState(false)
 
-let m = [1,2,3].slice(1,2) //вернет первый еэелемент с первой позиции
-//let b = m.splice(1,1) //если m, то вернет с первой поции 1 лемент, если b, то вернет все, кроме первой позиции первого элемента
-console.log(m)
+    let sss = ''
+    let strr = ''
+    let a = 0
+    console.log(sss)
+
+    function number(num) {
 
 
-   function number (num) {
-    let a = ''
-    console.log(test2)
-    if (test2.length >= test.length) {
-        //setTest2([...test2].splice(test2.length-1, test2.length - test.length == 0 ? 1 : (test2.length - test.length)-1))
-        setTest2([...test2].splice(2,1))
-    // }
+        if (test2.length >= test.length && test2.length > 0) {
+            let s = [...test2]
+            let m = s.splice(test.length - 1, test2.length - test.length + 1)
+        }
+
+
+        if (num !== '+' || num !== '-') {
+            if (num !== undefined && num !== '+' & num !== '-' & num !== '=') {
+                sss += num
+                console.log('Сложение чисел')
+            }
+        }
+
+        if (num == '+' || num == '-') {
+            setTest([...test, sss])
+            setTest2([...test2, num])
+            console.log('Добавление числа и знака в массив')
+        }
+
+
+        if (num == '=') {
+            if (test.length <= test2.length) {
+                setTest([...test, sss])
+            }
+        }
+        console.log(test, 'числа')
+        console.log(test2, 'знак')
+        console.log(num, 'получаемое значение')
+        console.log(sss, 'строка чисел')
     }
-    console.log(test2)
-    test2.map ((x,i)=> {
-            if (x === '+' ) {
-                if (a == '') {
-                    a = Number(test[i]) + Number(test[i+1])
-                    console.log(a, test[i], i)
+    useEffect(() => {
+        if (test.length > test2.length) {
+            test2.map((x, i) => {
+                if (x === '+') {
+                    if (a == 0) {
+                        a = 0
+                        a = Number(test[i]) + Number(test[i + 1])
+
+                    }
+                    else {
+                        a = Number(a) + Number(test[i + 1])
+                    }
+                }
+                if (x === '-') {
+                    if (a == 0) {
+                        a = Number(test[i]) - Number(test[i + 1])
+                    }
+                    else {
+                        a = Number(a) - Number(test[i + 1])
+                    }
+                }
+            })
+            for (let z = 0; z < test.length; z++) {
+                if (z < test.length && test2[z] !== undefined) {
+                    strr += String(test[z]) + String(test2[z])
                 }
                 else {
-                    console.log(a, i, test[i])
-                    a = a + Number(test[i+1])
-                    console.log(a, i)
+                    strr += String(test[z]) + '=' + String(a)
                 }
+
             }
-            if (x === '-' ) {
-                if (a == '') {
-                    a = Number(test[i]) - Number(test[i+1])
-                    console.log(a, test[i-1], test[i], i)
-                }
-                else {
-                    console.log(a, i, test[i])
-                    a = a - Number(test[i+1])
-                    console.log(a, i)
-                }
-            }
-            //console.log(x, i)
-    })
-    console.log(a)
-   }
-   number()
-  
+            console.log(a, 'Ответ') 
+            console.log(strr, 'Решение')
+            // setTest(a)
+            // setTest2([])
+        }
+    }, [test])
+
     return (
         <div>
-            <div style={{margin: '10px'}}>{str}</div>
-            <div><span style={{margin: '10px'}} onClick={(e)=>number([...mat, e.target.innerText])}>1</span><span style={{margin: '10px'}}>2</span><span style={{margin: '10px'}}>3</span><span style={{margin: '10px'}} onClick={(e)=>number([...mat, e.target.innerText])}>+</span></div>
-            <div><span style={{margin: '10px'}} onClick={(e)=>number([...mat, e.target.innerText])}>4</span><span style={{margin: '10px'}}>5</span><span style={{margin: '10px'}}>6</span><span style={{margin: '10px'}} onClick={(e)=>number(e.target.innerText)}>-</span></div>
-            <div><span style={{margin: '10px'}}>7</span><span style={{margin: '10px'}}>8</span><span style={{margin: '10px'}}>9</span><span style={{margin: '10px'}}>x</span></div>
-            <div><span style={{margin: '10px'}}>0</span><span style={{margin: '10px'}}>/</span><span style={{margin: '10px'}} onClick={(e)=>number(e.target.innerText)}>=</span></div>
+            <div style={{ margin: '10px' }}>{str}</div>
+            <div><span style={{ margin: '10px' }} onClick={(e) => number(e.target.innerText)}>1</span><span style={{ margin: '10px' }}>2</span><span style={{ margin: '10px' }}>3</span><span style={{ margin: '10px' }} onClick={(e) => number(e.target.innerText)}>+</span></div>
+            <div><span style={{ margin: '10px' }} onClick={(e) => number([...mat, e.target.innerText])}>4</span><span style={{ margin: '10px' }}>5</span><span style={{ margin: '10px' }}>6</span><span style={{ margin: '10px' }} onClick={(e) => number(e.target.innerText)}>-</span></div>
+            <div><span style={{ margin: '10px' }}>7</span><span style={{ margin: '10px' }}>8</span><span style={{ margin: '10px' }}>9</span><span style={{ margin: '10px' }}>x</span></div>
+            <div><span style={{ margin: '10px' }}>0</span><span style={{ margin: '10px' }}>/</span><span style={{ margin: '10px' }} onClick={(e) => number(e.target.innerText)}>=</span></div>
         </div>
     )
 }
